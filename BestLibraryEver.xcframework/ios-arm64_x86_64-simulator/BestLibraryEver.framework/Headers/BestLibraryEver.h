0ae54172077710bf1a0d191497e295c5a5f73cf0
Ilya Gryaznev <ambosodor576@ya.ru>
2022-02-07 19:41:03 +0400

    again

---
+++ BestLibraryEver.xcframework/ios-arm64_x86_64-simulator/BestLibraryEver.framework/Headers/BestLibraryEver.h
@@ -0,0 +1,18 @@
+//
+//  BestLibraryEver.h
+//  BestLibraryEver
+//
+//  Created by Ilya Gryaznev on 06.02.2022.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for BestLibraryEver.
+FOUNDATION_EXPORT double BestLibraryEverVersionNumber;
+
+//! Project version string for BestLibraryEver.
+FOUNDATION_EXPORT const unsigned char BestLibraryEverVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BestLibraryEver/PublicHeader.h>
+
+
